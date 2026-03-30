ae5c4bc87099614e2ec1e91be919a1dbe3f89258
tpuser <tp@tp.tp>
2026-03-30 17:10:37 +0200

    Settings helper

---
+++ app/src/main/java/com/example/jeu2048/settings/SettingsHelper.java
@@ -0,0 +1,29 @@
+package com.example.jeu2048.settings;
+
+public class SettingsHelper {
+    /*
+    Overall
+        Sound
+            Music (float)
+            Sound Effects (float)
+        Style
+            Theme (int)
+            Animation (bool)
+            Animation Speed (float)
+     SinglePlayer
+        Username (string)
+        GridSize (int, int)
+        Mode (int)
+            Target Score (int)
+            or
+            Time Limit (int)
+     Multiplayer
+        Username1 (string)
+        Username2 (string)
+        GridSize (int, int)
+        Mode (int)
+            Target Score (int)
+            or
+            Time Limit (int)
+     */
+}
