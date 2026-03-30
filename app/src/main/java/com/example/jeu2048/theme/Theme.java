240c679fe2070541397788dfa81c2e08e7d59288
tpuser <tp@tp.tp>
2026-03-30 17:30:22 +0200

    Theme and settings backend implemented

---
+++ app/src/main/java/com/example/jeu2048/theme/Theme.java
@@ -0,0 +1,33 @@
+package com.example.jeu2048.theme;
+
+import com.example.jeu2048.gameRender.GridRenderer;
+import com.example.jeu2048.gameRender.TileRenderer;
+import com.example.jeu2048.gameRender.gridRenderers.DefaultGridRenderer;
+import com.example.jeu2048.gameRender.tileRenderers.DefaultTileRenderer;
+
+public class Theme {
+    private GridRenderer gridRenderer;
+    private TileRenderer tileRenderer;
+
+    public Theme(ThemeName name) {
+        switch (name) {
+            case Classic:
+                gridRenderer = new DefaultGridRenderer();
+                tileRenderer = new DefaultTileRenderer();
+                break;
+
+            default:
+                gridRenderer = new DefaultGridRenderer();
+                tileRenderer = new DefaultTileRenderer();
+                break;
+        }
+    }
+
+    public GridRenderer getGridRenderer() {
+        return gridRenderer;
+    }
+
+    public TileRenderer getTileRenderer() {
+        return tileRenderer;
+    }
+}
